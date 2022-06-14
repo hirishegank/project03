5f55c5260fa51b11773f3fc13769d86b7d56cfe8
Hirishegan Karuneswaran <32614969+hirishegank@users.noreply.github.com>
2022-06-14 22:46:54 +0100

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,80 @@
+$(document).ready(function(){
+    
+    (function($) {
+        "use strict";
+
+    
+    jQuery.validator.addMethod('answercheck', function (value, element) {
+        return this.optional(element) || /^\bcat\b$/.test(value)
+    }, "type the correct answer -_-");
+
+    // validate contactForm form
+    $(function() {
+        $('#contactForm').validate({
+            rules: {
+                name: {
+                    required: true
+                },
+                subject: {
+                    required: true
+                },
+                number: {
+                    required: true
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true,
+                    minlength: 20
+                }
+            },
+            messages: {
+                name: {
+                    required: "Please enter your name.",
+                },
+                subject: {
+                    required: "Please enter a subject.",
+                },
+                number: {
+                    required: "Please enter your phone number."
+                },
+                email: {
+                    required: "Please enter a valid email address."
+                },
+                message: {
+                    required: "Please Enter message.",
+                    minlength: "Your message should contain atleast 20 characters."
+                    
+                }
+            },
+            submitHandler: function(form) {
+                $(form).ajaxSubmit({
+                    type:"POST",
+                    data: $(form).serialize(),
+                    url:"contact_process.php",
+                    success: function() {
+                        $('#contactForm :input').attr('disabled', 'disabled');
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $(this).find(':input').attr('disabled', 'disabled');
+                            $(this).find('label').css('cursor','default');
+                            $('#success').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#success').modal('show');
+                        })
+                    },
+                    error: function() {
+                        $('#contactForm').fadeTo( "slow", 1, function() {
+                            $('#error').fadeIn()
+                            $('.modal').modal('hide');
+		                	$('#error').modal('show');
+                        })
+                    }
+                })
+            }
+        })
+    })
+        
+ })(jQuery)
+})
